7b6203bb7d769691d1632b905a6387f22589f85e
Valery Vokhmin <valert917@gmail.com>
2023-01-29 11:22:35 +0300

    sem1 task4 try1 fix

--- Python/sem1/task_4.py
+++ Python/sem1/task_4.py
@@ -16,8 +16,7 @@ div400 = year % 400 == 0
 div4 = year % 4 == 0
 div100 = year % 100 == 0
 
+res = 'NO'
 if div400 or (div4 and not div100):
     res = 'YES'
-else:
-    res = 'NO'
 print(f'Високосный: {res}')
