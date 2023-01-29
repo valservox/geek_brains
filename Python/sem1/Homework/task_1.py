e58755ffe9757b6dd6c0ed16c30cb018e090935b
Valery Vokhmin <valert917@gmail.com>
2023-01-29 14:10:18 +0300

    sem1 hw task1 try1

--- Python/sem1/Homework/task_1.py
+++ Python/sem1/Homework/task_1.py
@@ -5,3 +5,9 @@
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0)
 '''
+
+sum = 0
+num = input('Введите число: ')
+for i in range(len(num)):
+    sum += int(num[i])
+print(f'Сумма цифр числа {num}: {sum}')
